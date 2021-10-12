f275a400f2c86e5577e66e6f520f05c80c1a79ee
Bread White <breadrock1@gmail.com>
2021-10-12 20:18:21 +0300

    Has been added try/catch block into get method SpeechpadHandler class

--- src/main/java/server/handler/speechpad/SpeechpadHandler.java
+++ src/main/java/server/handler/speechpad/SpeechpadHandler.java
@@ -78,8 +78,12 @@ public class SpeechpadHandler {
         @Query("speechpad_id") String speechpadId
     ) throws NoSuchSpeechpadException {
         logger.info("Handle get speechpad by id");
-        Speechpad speechpad = speechpadManager.getSpeechpad(speechpadId);
-        return new SpeechpadCreateResponse(speechpad.getId());
+        try {
+            Speechpad speechpad = speechpadManager.getSpeechpad(speechpadId);
+            return new SpeechpadCreateResponse(speechpad.getId());
+        } catch (NoSuchSpeechpadException e) {
+            throw new NoSuchSpeechpadException(speechpadId);
+        }
     }
 
 }
