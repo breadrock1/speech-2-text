f1a1ac1db3a8c58bf0433117c24c13021db1d013
Bread White <breadrock1@gmail.com>
2021-10-13 23:06:39 +0300

    Tryig fix http-method getAll

--- src/main/java/server/response/speechpad/SpeechpadGetAllResponse.java
+++ src/main/java/server/response/speechpad/SpeechpadGetAllResponse.java
@@ -1,16 +1,17 @@
 package server.response.speechpad;
 
+import javax.annotation.Nullable;
 import server.response.GenericResponse;
 import server.speechpad.Speechpad;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class SpeechpadGetAllResponse extends GenericResponse {
 
-    private final List<Speechpad> speechpads;
+    @Nullable
+    public final List<Speechpad> speechpads;
 
-    public SpeechpadGetAllResponse(List<Speechpad> speechpads) {
+    public SpeechpadGetAllResponse(@Nullable final List<Speechpad> speechpads) {
         this.speechpads = speechpads;
     }
 }
