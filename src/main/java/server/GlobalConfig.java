a77fc47b45c9050854031edc4449a6dd9a818761
Bread White <breadrock1@gmail.com>
2021-10-17 15:35:31 +0300

    Has been added bucket name suffix

--- src/main/java/server/GlobalConfig.java
+++ src/main/java/server/GlobalConfig.java
@@ -5,8 +5,8 @@ public class GlobalConfig {
     private GlobalConfig() {
     }
 
-    public static final String AUDIO_BUCKET_NAME = "summary-audio-files";
-    public static final String META_BUCKET_NAME = "summary-meta-files";
-    public static final String RESULT_BUCKET_NAME = "summary-result-files";
-    public static final String ARCHIVED_CONFERENCE_BUCKET_NAME = "summary-archived-conferences";
+    public static final String AUDIO_BUCKET_NAME = "summary-audio-files-speech-to-text-demo-329208";
+    public static final String META_BUCKET_NAME = "summary-meta-files-speech-to-text-demo-329208";
+    public static final String RESULT_BUCKET_NAME = "summary-result-files-speech-to-text-demo-329208";
+    public static final String ARCHIVED_CONFERENCE_BUCKET_NAME = "summary-archived-conferences-speech-to-text-demo-329208";
 }
