cef434f622f069d4200c9adcbe9197ffbc774815
Bread White <breadrock1@gmail.com>
2021-10-22 00:10:48 +0300

    Has been added ability to store speechpad data into database

--- src/main/java/server/handler/speechpad/SpeechpadHandler.java
+++ src/main/java/server/handler/speechpad/SpeechpadHandler.java
@@ -13,17 +13,14 @@ import server.logging.Logger;
 import server.response.GenericResponse;
 import server.response.speechpad.SpeechpadChunkResponse;
 import server.response.speechpad.SpeechpadCreateResponse;
-import server.response.speechpad.SpeechpadGetResponse;
+import server.response.speechpad.SpeechpadFullObjectResponse;
 import server.response.speechpad.SpeechpadListResponse;
-import server.response.speechpad.SpeechpadRenameResponse;
-import server.response.transcribe.TranscribeResult;
 import server.speechpad.NoSuchSpeechpadException;
 import server.speechpad.Speechpad;
 import server.speechpad.SpeechpadManager;
 
 import javax.inject.Inject;
 import java.io.IOException;
-import java.util.List;
 import java.util.Locale;
 
 import static server.response.ErrorResponse.errorResponse;
@@ -47,7 +44,7 @@ public class SpeechpadHandler {
         @Query("name") String name
     ) {
         logger.info("Handle create speechpad");
-        Speechpad speechpad = speechpadManager.create(model, name);
+        Speechpad speechpad = speechpadManager.createSpeechpad(model, name);
         return new SpeechpadCreateResponse(speechpad.getId(), speechpad.getName());
     }
 
@@ -58,24 +55,8 @@ public class SpeechpadHandler {
     ) throws NoSuchSpeechpadException {
         logger.info("Handle remove speechpad");
         try {
-            speechpadManager.delete(speechpadId);
+            speechpadManager.deleteSpeechpad(speechpadId);
             return new GenericResponse(true);
-        } catch (NoSuchSpeechpadException e) {
-            throw new NoSuchSpeechpadException(speechpadId);
-        }
-    }
-
-    @Description("Переименование голосового блокнота")
-    @HandlePost("/rename")
-    SpeechpadRenameResponse rename(
-        @Query("speechpad_id") String speechpadId,
-        @Query("new_name") String newName
-    ) throws NoSuchSpeechpadException {
-        logger.info("Handle rename speechpad");
-        try {
-            Speechpad speechpad = speechpadManager.getSpeechpad(speechpadId);
-            speechpad.setName(newName);
-            return new SpeechpadRenameResponse(speechpad.getId(), speechpad.getName());
         } catch (NoSuchSpeechpadException e) {
             throw new NoSuchSpeechpadException(speechpadId);
         }
@@ -98,13 +79,13 @@ public class SpeechpadHandler {
 
     @Description("Получение голосового блокнота по идентификатору")
     @HandleGet("/get")
-    SpeechpadGetResponse get(
+    SpeechpadFullObjectResponse get(
         @Query("speechpad_id") String speechpadId
     ) throws NoSuchSpeechpadException {
         logger.info("Handle get speechpad by id");
         try {
             Speechpad speechpad = speechpadManager.getSpeechpad(speechpadId);
-            return new SpeechpadGetResponse(speechpad.getId(), speechpad.getName(), speechpad.getTranscribe());
+            return new SpeechpadFullObjectResponse(speechpad.getId(), speechpad.getName(), speechpad.getTranscribe());
         } catch (NoSuchSpeechpadException e) {
             throw new NoSuchSpeechpadException(speechpadId);
         }
@@ -117,50 +98,33 @@ public class SpeechpadHandler {
         return new SpeechpadListResponse(speechpadManager.getAllSpeechpads());
     }
 
-    @Description("Получение транскрипции голосового блокнота по идентификатору")
-    @HandleGet("/result")
-    SpeechpadChunkResponse result(
-        @Query("speechpad_id") String speechpadId
+    @Description("Переименование голосового блокнота")
+    @HandlePost("/rename")
+    SpeechpadCreateResponse rename(
+        @Query("speechpad_id") String speechpadId,
+        @Query("new_name") String newName
     ) throws NoSuchSpeechpadException {
-        logger.info("Handle get speechpad transcribe result by id");
+        logger.info("Handle rename speechpad");
         try {
-            Speechpad speechpad = speechpadManager.getSpeechpad(speechpadId);
-            return new SpeechpadChunkResponse(speechpad.getTranscribe());
+            Speechpad speechpad = speechpadManager.renameSpeechpad(speechpadId, newName);
+            return new SpeechpadCreateResponse(speechpad.getId(), speechpad.getName());
         } catch (NoSuchSpeechpadException e) {
             throw new NoSuchSpeechpadException(speechpadId);
         }
     }
 
     @Description("Изменение транскрипции голосового блокнота")
-    @HandlePost("/edit")
-    SpeechpadChunkResponse edit(
+    @HandlePost("/update")
+    SpeechpadFullObjectResponse update(
         @Query("speechpad_id") String speechpadId,
         @Query("transcribe") String data
     ) throws NoSuchSpeechpadException {
-        logger.info("Handle edit speechpad transcribe");
+        logger.info("Handle update speechpad transcribe");
         try {
-            Speechpad speechpad = speechpadManager.getSpeechpad(speechpadId);
-            List<TranscribeResult> result = speechpad.update(new TranscribeResult(data));
-            return new SpeechpadChunkResponse(result);
+            Speechpad speechpad = speechpadManager.editSpeechpadTranscribe(speechpadId, data);
+            return new SpeechpadFullObjectResponse(speechpad.getId(), speechpad.getName(), speechpad.getTranscribe());
         } catch (NoSuchSpeechpadException e) {
             throw new NoSuchSpeechpadException(speechpadId);
-        }
-    }
-
-
-    @Description("Изменение транскрипции голосового блокнота")
-    @HandlePost("/store")
-    GenericResponse store(
-            @Query("speechpad_id") String speechpadId
-    ) throws NoSuchSpeechpadException {
-        logger.info("Handle store speechpad transcribe");
-        try {
-            Speechpad speechpad = speechpadManager.getSpeechpad(speechpadId);
-            System.out.println(speechpad.getId());
-            speechpadManager.storeSpeechpadToDatabase(speechpad);
-            return new GenericResponse(true);
-        } catch (NoSuchSpeechpadException e) {
-            throw new NoSuchSpeechpadException("Failed");
         }
     }
 
